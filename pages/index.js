64f19b6d3cb60c49e3b1a1dee98dd06447d5c5d4
Serafettin Bayram <sbayram2016@gmail.com>
2020-06-18 13:54:56 +0200

    connected db

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
-function Home() {
+import { useEffect } from 'react';
+import axios from 'axios';
+
+function Home({ products }) {
+  console.log(products);
+  const getProducts = async () => {
+    const url = 'http://localhost:3000/api/products';
+    const response = await axios.get(url);
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return <>home</>;
 }
+
+Home.getInitialProps = async () => {
+  // fetch data on server
+  const url = 'http://localhost:3000/api/products';
+  const response = await axios.get(url);
+  return { products: response.data };
+  // return response data as an object
+  // note: this object will be merged with existing props
+};
 
 export default Home;
