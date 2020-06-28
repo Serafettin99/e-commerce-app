81619087b29c1cadc1b5b9f6867236a651cd5ae4
Serafettin Bayram <sbayram2016@gmail.com>
2020-06-28 20:12:28 +0200

    Set a  baseUrl, Created Delete product middleware

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList';
-
+import baseUrl from '../utils/baseUrl';
 function Home({ products }) {
   return <ProductList products={products} />;
 }
@@ -9,7 +9,7 @@ function Home({ products }) {
 // So in order to use this home function we have to execute it as a kind of method on the component.
 Home.getInitialProps = async () => {
   // fetch data on server
-  const url = 'http://localhost:3000/api/products';
+  const url = `${baseUrl}/api/products`;
   const response = await axios.get(url);
   return { products: response.data };
   // return response data as an object
--- pages/product.js
+++ pages/product.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 import ProductSummary from '../components/Product/ProductSummary';
 import ProductAttributes from '../components/Product/ProductAttributes';
+import baseUrl from '../utils/baseUrl';
 
 export const Product = ({ product }) => {
-  console.log({ product });
   return (
     <>
       <ProductSummary {...product} />
@@ -13,7 +13,7 @@ export const Product = ({ product }) => {
 };
 
 Product.getInitialProps = async ({ query: { _id } }) => {
-  const url = 'http://localhost:3000/api/product';
+  const url = `${baseUrl}/api/product`;
   const payload = { params: { _id } };
   const response = await axios.get(url, payload);
   return { product: response.data };
