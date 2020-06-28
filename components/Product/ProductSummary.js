cbe571221d59aa423d8d8a9c5b3ebc584f1d64d5
Serafettin Bayram <sbayram2016@gmail.com>
2020-06-28 17:26:41 +0200

    Product page styled rendering errors fixed

--- components/Product/ProductSummary.js
+++ components/Product/ProductSummary.js
@@ -1,5 +1,26 @@
-function ProductSummary() {
-  return <>ProductSummary</>;
-}
+import { Item, Label } from 'semantic-ui-react';
+import AddProductToCard from './AddProductToCart';
+
+const ProductSummary = ({ name, mediaUrl, _id, price, sku }) => {
+  return (
+    <>
+      <Item.Group>
+        <Item>
+          <Item.Image size='medium' src={mediaUrl} />
+          <Item.Content>
+            <Item.Header>{name}</Item.Header>
+            <Item.Description>
+              <p>${price}</p>
+              <Label>SKU: {sku}</Label>
+            </Item.Description>
+            <Item.Extra>
+              <AddProductToCard productId={_id} />
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+      </Item.Group>
+    </>
+  );
+};
 
 export default ProductSummary;
