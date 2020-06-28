76a6e94f23513f2f2739185a18aff653dcdef85b
Serafettin Bayram <sbayram2016@gmail.com>
2020-06-28 14:49:28 +0200

    Product Cards built, Components made responsive

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,9 @@
 import { useEffect } from 'react';
 import axios from 'axios';
+import ProductList from '../components/Index/ProductList';
 
 function Home({ products }) {
-  console.log(products);
-  // const getProducts = async () => {
-  //   const url = 'http://localhost:3000/api/products';
-  //   const response = await axios.get(url);
-  //   console.log(response.data);
-  // };
-
-  // useEffect(() => {
-  //   getProducts();
-  // }, []);
-
-  return <>home</>;
+  return <ProductList products={products} />;
 }
 
 // So in order to use this home function we have to execute it as a kind of method on the component.
