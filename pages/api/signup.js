df7bcfa3ede1f3c1c67e1b2290a210c3ddc354d5
Serafettin Bayram <sbayram2016@gmail.com>
2020-07-01 22:27:25 +0200

    Validation added to serverside

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -2,6 +2,8 @@ import connectDb from '../../utils/connectDb';
 import User from '../../models/User';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import isEmail from 'validator/lib/isEmail';
+import isLength from 'validator/lib/isLength';
 
 connectDb();
 
@@ -9,14 +11,23 @@ export default async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
+    // 1) Validate name / email / password
+    if (!isLength(name, { min: 3, max: 17 })) {
+      return res.status(422).send('Name must be 3=17 characters long');
+    } else if (!isLength(password, { min: 6 })) {
+      return res.status(422).send('Password must be at least 6 characters');
+    } else if (!isEmail(email)) {
+      return res.status(422).send('Email must be valid');
+    }
+
     const user = await User.findOne({ email });
-    // 1) Check if the user exists
+    // 2) Check if the user exists
     if (user) {
       return res.status(422).send(`User already exists with email ${email}`);
     }
-    // 2) if not, hash their password
+    // 3) if not, hash their password
     const hash = await bcrypt.hash(password, 10);
-    // 3) Create user
+    // 4) Create user
     const newUser = await new User({
       name,
       email,
@@ -24,11 +35,11 @@ export default async (req, res) => {
     }).save();
 
     console.log({ newUser });
-    // 4) Create token for the new user
+    // 5) Create token for the new user
     const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: '7d',
     });
-    // 5) Send back
+    // 6) Send back
     res.status(201).json(token);
   } catch (error) {
     console.error(error);
